f5999041bd455bcf8879144117e9c415ba62b27b
Daniel-DY-Lee <do@ualberta.ca>
2016-12-04 18:08:14 -0700

    last changes before heading out

--- do/simulator.c
+++ do/simulator.c
@@ -5,6 +5,7 @@
 /* Used code from: 
 	http://www.sanfoundry.com/c-program-implement-hash-tables-chaining-with-singly-linked-lists/ */
 
+
 struct hash * hashTable = NULL;
 
 
@@ -12,12 +13,12 @@ struct node {
 	unsigned int address;
 	int value;
 	struct node *next;
-}
+};
 
 
 struct hash {
 	struct node *head;
-}
+};
 
 
 struct node * createNode (unsigned int address, int value) {
@@ -27,26 +28,49 @@ struct node * createNode (unsigned int address, int value) {
 	newnode -> value = value;
 	newnode -> next = NULL;
 	return newnode;
+};
+
+
+int pageSize;
+int windowSize;
+int totalAccess = 0;
+int *pageCount;
+
+void init (int psize, int winsize) {
+	// do nothing
+	pageSize = psize;
+	windowSize = winsize;
+	int pageArray[psize];
+	pageCount = (int *)pageArray[psize];
 }
 
 
-void init (int psize, int winsize) {
+int hashFunction (unsigned int address, int value) {
 
-	// do nothing
+	int bucket = 0;
 
+	totalAccess ++;
+	/* If totalAccess = winsize then do something */
+
+	printf ("%d %d %d \n", windowSize, totalAccess, pageSize);
+	pageCount[0]++;
+	printf("%d %d\n", pageCount[0], pageCount[1]);
+	return bucket;
 }
 
 
 void put (unsigned int address, int value) {
 
 	/* From hashtable.c, void insertToHash */
+	int bucket;
+	bucket = hashFunction(address, value);
 
 }
 
 
 int get (unsigned int address) {
 
-	int value;
+	int value = 0;
 	/* From hashtable.c, void searchInHash */
 	return value;
 
@@ -69,6 +93,7 @@ int main(int argc, char const *argv[])
 	// void process ();
 
 	init (128, 1000);
+	put (1, 1);
 
 	return 0;
 }
